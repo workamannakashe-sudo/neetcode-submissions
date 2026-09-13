6359f8f9606caf2055dcd1855289350430d0beca
workamannakashe-sudo <work.amannakashe@gmail.com>
2026-09-13 15:37:55 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int sum = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                sum = nums[i] + nums[j];
+
+                if (sum == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+}
